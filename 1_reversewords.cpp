5992d29e8a9cd22d95ed99d01bef78f7a4ad4d45
leftovermuffin <62449008+leftovermuffin@users.noreply.github.com>
2020-06-05 22:25:55 +0600

    Update 1_reversewords.cpp

--- 1_reversewords.cpp
+++ 1_reversewords.cpp
@@ -2,9 +2,52 @@
 REVERSE WORDS
 inp: now youre just somebody that I used to know
 out: know to used I that somebody just youre now
-
-this is not the in-place solution
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rev(string& s, int l, int r){
+    while(l<r){
+        swap(s[l], s[r]);
+        l++;
+        r--;
+    }
+}
+
+int main() {
+    string inp;
+    getline(cin, inp);
+    string out = "";
+    int len = inp.length();
+    
+    //rewind time!
+    rev(inp, 0, len-1);
+    
+    int startpos = 0;
+    for(int i=0 ; i<=len ; i++){
+        if(inp[i]==' ' or i==len){
+            rev(inp, startpos, i-1);
+            startpos = i+1;
+        }
+    }
+    
+    cout << inp << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+this is not the in-place solution
+
 
 #include <bits/stdc++.h>
 
@@ -45,3 +88,5 @@ int main() {
     
     return 0;
 }
+
+*/
